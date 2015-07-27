13bb63b31f656794e8d215a47ce9e9fec846142d
Bruno Assis <brunoassis@gmail.com>
2015-07-27 14:26:17 -0300

    Changed system aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,4 +1,4 @@
 alias open="xdg-open"
 alias lh="ls -lhart"
+alias ll="ls -l"
 alias update="sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y autoremove && sudo apt-get -y clean"
-alias curltime="curl -ivw \"@/home/bruno/curltime_output.txt\""
