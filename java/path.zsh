d14dffca816cae3f6d8e9b220f130b221109486a
Bruno Assis <brunoassis@gmail.com>
2015-09-07 13:34:57 -0300

    Fixed JAVA_HOME for Mac

--- java/path.zsh
+++ java/path.zsh
@@ -1,6 +1,6 @@
 if [[ "$PLATFORM" == "Darwin" ]]
 then
-  export JAVA_HOME="/usr"
+  export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
 else
   export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 fi
