f0cd820771033eff3b514e670b963dce7a0d73c9
Bruno Assis <brunoassis@gmail.com>
2015-09-07 13:51:55 -0300

    Fixed Mac Maven Home

--- java/path.zsh
+++ java/path.zsh
@@ -1,5 +1,8 @@
 if [[ "$PLATFORM" == "Darwin" ]]
 then
+  export M2_HOME="/usr/local/Cellar/maven/3.3.3/libexec"
+  export M2="$M2_HOME/bin"
+  export PATH="$PATH:$M2"
   export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
 else
   export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
