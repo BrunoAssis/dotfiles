bfda36886445c43d704ed231bc6203e9388889b0
Bruno Assis <brunoassis@gmail.com>
2017-03-30 18:15:53 -0300

    [Docker] Clearing dangling volumes on docker-cleanup

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -3,6 +3,7 @@ alias dc="docker-compose"
 docker-cleanup() {
     docker ps -q -a | xargs docker rm
     docker rmi $(docker images | grep "^<none>" | awk '{print $3}')
+    docker volume rm $(docker volume ls -qf dangling=true)
 }
 
 alias dspec="docker-compose run web bundle exec rspec"
