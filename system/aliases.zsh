913a715100045c59b35cdfffb14df842f7a0c720
Bruno Assis <brunoassis@gmail.com>
2021-01-18 21:55:10 -0300

    fix ls on mac

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -2,9 +2,9 @@
 if [[ "$PLATFORM" == "Linux" ]]
 then
   alias open="xdg-open"
+  alias ls="ls --color=auto"
 fi
 
-alias ls="ls --color=auto"
 alias lh="ls -lhart"
 alias ll="ls -l"
 alias flushdns='sudo discoveryutil udnsflushcaches'
