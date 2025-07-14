d3c7b0d1747dd6236e3c62f4bde59c66e4368db3
Bruno Assis <brunoassis@gmail.com>
2025-07-14 14:27:22 -0300

    change docker aliases and autocomplete

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,9 +1,8 @@
-alias dc="docker-compose"
+alias dc="docker compose"
+alias dcw="docker compose up --watch"
 
 docker-cleanup() {
     docker ps -q -a | xargs docker rm
     docker rmi $(docker images | grep "^<none>" | awk '{print $3}')
     docker volume rm $(docker volume ls -qf dangling=true)
 }
-
-alias dspec="docker-compose run web bundle exec rspec"
